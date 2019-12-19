f72459f7daaa9879355c8ba7443cb7c0f0e128c1
USUARIO 03 <USUARIO 03@DESKTOP-2BVO22Q>
2019-12-18 20:22:46 -0500

    Inicio

---
+++ src/practica3/usaPractica3.java
@@ -0,0 +1,14 @@
+
+package practica3;
+
+
+public class usaPractica3 {
+
+ 
+    public static void main(String[] args) {
+        Practica3 c = new Practica3();
+        c.setVisible(true);
+        c.setLocationRelativeTo(null);
+    }
+    
+}
